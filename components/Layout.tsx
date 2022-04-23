843a36d08f2502f7b474377dbb693c258be27025
hoshico <melancholinista.yasu@gmail.com>
2022-04-23 18:25:25 +0900

    add: footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
+import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout({ children, title = 'hoshi-code' }) {
   return (
-    <div className='bg-gradient-to-br from-primary pb-64 to-secondary'>
+    <div className='bg-gradient-to-br from-primary to-secondary'>
       <Header />
       {children}
+      <Footer />
     </div>
   )
 }

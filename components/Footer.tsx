53b01dd1e5a0ae102ef3feffd60d279961de8d85
hoshico <melancholinista.yasu@gmail.com>
2022-05-01 10:54:35 +0900

    fix Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,25 +7,8 @@ export default function Footer() {
         {/*<p>ACME Industries Ltd.Providing reliable tech since 1992</p>*/}
       </div> 
       <div>
-        {/*<span className="footer-title">Services</span> 
-        <a className="link link-hover">Branding</a> 
-        <a className="link link-hover">Design</a> 
-        <a className="link link-hover">Marketing</a> 
-        <a className="link link-hover">Advertisement</a>*/}
+        <span className="font-bold">Copyright © 2022hoshico.dev All rights reserved.</span> 
       </div> 
-      <div>
-        {/*<span className="footer-title">Company</span> 
-        <a className="link link-hover">About us</a> 
-        <a className="link link-hover">Contact</a> 
-        <a className="link link-hover">Jobs</a> 
-        <a className="link link-hover">Press kit</a>*/}
-      </div> 
-      <div>
-        {/*<span className="footer-title">Legal</span> 
-        <a className="link link-hover">Terms of use</a> 
-        <a className="link link-hover">Privacy policy</a> 
-        <a className="link link-hover">Cookie policy</a>*/}
-      </div>
     </footer>
   )
 } 
--- components/Main.tsx
+++ components/Main.tsx
@@ -4,7 +4,7 @@ import Header from './Header'
 export default function Main({children}) {
 
   return (
-    <div className="w-4/5 bg-base-200 my-4 rounded m-auto">
+    <div className="w-4/5 bg-base-200 my-4 rounded-xl m-auto">
       <div className="mx-10 my-10">
         {children}
       </div>

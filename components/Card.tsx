4160c47aa56e36da148ff4e9fdf2807a48956ec8
hoshico <melancholinista.yasu@gmail.com>
2022-05-03 13:51:04 +0900

    add fragment to some pages

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,50 +10,52 @@ type Card = {
 export default function Card(props: Card) {
   const [mouseOver, setMouseOver] = useState(false);
   return (
-    <Link href={`/blog/${props.blog.id}`}>
-      <a>
-        <div
-          className="w-full shadow-xl card bg-base-100"
-          onMouseOver={() => setMouseOver(true)}
-          onMouseLeave={() => setMouseOver(false)}>
-          <figure className="overflow-hidden h-32 lg:h-40">
-            <img
-              className={
-                mouseOver
-                  ? 'object-cover w-full h-full scale-110 transition'
-                  : 'object-cover w-full h-full transition'
-              }
-              src={
-                props.blog.photo
-                  ? props.blog.photo.url
-                  : 'https://api.lorem.space/image/shoes'
-              }
-              alt="blogイメージ"
-            />
-          </figure>
-          <div className="card-body">
-            <Moment className="text-xs" format="YYYY/MM/DD">
-              {props.blog.publishedAt}
-            </Moment>
-            <h2 className="text-sm card-title sm:text-md">
-              {props.blog.title}
-            </h2>
-            {/* １ヶ月以内の投稿はNEWのバッジをつける */}
-            {/*<div className="badge badge-secondary">NEW</div>*/}
-            <div className="justify-end card-actions">
-              {props.blog.category ? (
-                props.blog.category.map(category => (
-                  <div key={category.id} className="badge badge-outline">
-                    <p >{category.name}</p>
-                  </div>
-                ))
-              ) : (
-                <div className="badge badge-outline">その他</div>
-              )}
+    <>
+      <Link href={`/blog/${props.blog.id}`}>
+        <a>
+          <div
+            className="w-full shadow-xl card bg-base-100"
+            onMouseOver={() => setMouseOver(true)}
+            onMouseLeave={() => setMouseOver(false)}>
+            <figure className="overflow-hidden h-32 lg:h-40">
+              <img
+                className={
+                  mouseOver
+                    ? 'object-cover w-full h-full scale-110 transition'
+                    : 'object-cover w-full h-full transition'
+                }
+                src={
+                  props.blog.photo
+                    ? props.blog.photo.url
+                    : 'https://api.lorem.space/image/shoes'
+                }
+                alt="blogイメージ"
+              />
+            </figure>
+            <div className="card-body">
+              <Moment className="text-xs" format="YYYY/MM/DD">
+                {props.blog.publishedAt}
+              </Moment>
+              <h2 className="text-sm card-title sm:text-md">
+                {props.blog.title}
+              </h2>
+              {/* １ヶ月以内の投稿はNEWのバッジをつける */}
+              {/*<div className="badge badge-secondary">NEW</div>*/}
+              <div className="justify-end card-actions">
+                {props.blog.category ? (
+                  props.blog.category.map(category => (
+                    <div key={category.id} className="badge badge-outline">
+                      <p >{category.name}</p>
+                    </div>
+                  ))
+                ) : (
+                  <div className="badge badge-outline">その他</div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </>
   );
 }

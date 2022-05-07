348d6dbc3b13ecb4503aa56bc6494241d414919c
hoshico <melancholinista.yasu@gmail.com>
2022-05-07 18:57:29 +0900

    add types to client.ts

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: '2525hoshi',
-  apiKey: process.env.API_KEY ,
+  serviceDomain: '2525hoshi' || '',
+  apiKey: process.env.API_KEY || '',
 });

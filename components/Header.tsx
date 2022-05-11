a262d35f8963e47eabc46512a8d7181a89140396
hoshico <melancholinista.yasu@gmail.com>
2022-05-12 08:44:04 +0900

    add types

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,21 +5,6 @@ export default function Header() {
   return (
     <header className="text-gray-600 bg-blue-500 body-font">
       <div className="navbar bg-base-300">
-        {/*<div className="flex-none">
-          <button className="btn btn-square btn-ghost">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-5 h-5 stroke-current">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </button>
-        </div>*/}
         <div className="flex-1">
           <Link href="/">
             <a className="ml-2 md:ml-5 text-3xl normal-case btn btn-ghost">

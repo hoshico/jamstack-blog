f9815f13cf3489b895c8b3ab84aac181e534108b
hoshico <melancholinista.yasu@gmail.com>
2022-06-20 13:24:54 +0900

    remove image from about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -12,9 +12,6 @@ export default function About ({profile}: AboutProps) {
       <div className="w-4/5 mt-6 p-10 mx-auto bg-base-300 rounded-md">
         <p className="text-center text-md font-bold">プロフィール</p>
         <div className="image-box relative w-20 mx-auto">
-          {/*<Image
-        
-          ></Image>*/}
         </div>
         <div className="w-full mt-4">
           <p>名前: {profile.name}</p>

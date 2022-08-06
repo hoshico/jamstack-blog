ad184aeb448efe1fbee5ef7fb58d8c39fc5ebeba
hoshico <melancholinista.yasu@gmail.com>
2022-08-06 19:25:39 +0900

    add profile-img

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable tailwindcss/no-custom-classname */
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 import { Profile } from '../components/types/Profile';
 import { client } from '../libs/client';
 
@@ -6,8 +8,12 @@ type AboutProps = {
   profile: Profile;
 };
 export default function About({ profile }: AboutProps) {
+  console.log(profile.image.url);
   return (
     <div className="w-4/5 mt-6 p-10 mx-auto bg-base-300 rounded-md">
+      <div className="flex justify-center mb-2">
+        <Image src={profile.image.url} width={100} height={100} />
+      </div>
       <p className="text-center text-md font-bold">プロフィール</p>
       <div className="image-box relative w-20 mx-auto"></div>
       <div className="w-full mt-4">

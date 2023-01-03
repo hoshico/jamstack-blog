41a2bbbc69eaf3a6e39c8028a461789fda915fa5
hoshico <melancholinista.yasu@gmail.com>
2023-01-03 21:29:46 +0900

    add three.js to profile

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,26 +3,44 @@ import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import { Profile } from '../components/types/Profile';
 import { client } from '../libs/client';
+import Bath from '../components/Bath';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Text } from '@react-three/drei';
+// 「Vector3」のインポートを追加
+import { Mesh, Vector3 } from 'three';
 
 type AboutProps = {
   profile: Profile;
 };
+const Rig = ({ v = new Vector3() }) => {
+  return useFrame((state) => {
+    state.camera.position.lerp(v.set(state.mouse.x / 2, state.mouse.y / 2, 10), 0.05)
+  })
+};
+
 export default function About({ profile }: AboutProps) {
   return (
     <div className="w-4/5 mt-6 p-10 mx-auto bg-base-300 rounded-md">
-      <div className="flex justify-center mb-2">
-        <Image src={profile.image.url} width={200} height={200}/>
+      <div className="h-[200px]">
+        <Canvas>
+          <ambientLight />
+          <Bath position={[0, -2, 5]} />
+          <Rig />
+          <pointLight position={[10, 10, 10]} />
+          <Text
+            position={[0, 1, 0]}
+            font="/Roboto-Black.ttf"
+            fontSize={3}
+            color={'#222'}>
+            Profile
+          </Text>
+        </Canvas>
       </div>
-      <p className="text-center text-md font-bold">プロフィール</p>
       <div className="image-box relative w-20 mx-auto"></div>
       <div className="w-full mt-4">
-        <p>名前: {profile.name}</p>
-        <p className="mt-4">{profile.main}</p>
-        <a
-          href="https://qiita.com/yasu-hoshi"
-          className="block mt-4 text-md hover:opacity-75">
-          qiitaでも投稿してます。
-        </a>
+        <p>name: Hoshi</p>
+        <p className="mt-4">東京在住の駆け出しエンジニア。</p>
+        <p>好きなもの: 銭湯/MLB</p>
       </div>
     </div>
   );

71ccb0c72874fe8dab24d23db9e98e0988265b28
hoshico <melancholinista.yasu@gmail.com>
2023-01-08 11:24:03 +0900

    change favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,8 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/png" sizes="32x32" href="static/favicon/hoshico32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="static/favicon/hoshico16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="static/favicon/favicon32.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="static/favicon/favicon16.ico" />
         </Head>
         <body>
           <Main />

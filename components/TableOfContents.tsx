643b996623f79d1ba59844f4730a89e60667b216
hoshico <melancholinista.yasu@gmail.com>
2023-01-29 15:47:50 +0900

    apply format

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -5,20 +5,20 @@ type Toc = {
 };
 type TocProps = {
   toc: Array<Toc>;
-}
-export default function TableOfContents({toc}: TocProps) {
+};
+export default function TableOfContents({ toc }: TocProps) {
   return (
     <div className="hidden sticky top-10 py-10 px-2 w-[250px] h-[fit-content] rounded-xl shadow md:block bg-base-100">
       <p className="pl-2 mb-5 font-bold">目次</p>
       <ul className="pl-6 list-disc">
         {toc.map(data => (
-          <li className="list-none mb-2 font-bold opacity-70 hover:opacity-100" key={data.id}>
-            <a href={`#${data.id}`}>
-              {data.text}
-            </a>
+          <li
+            className="list-none mb-2 font-bold opacity-70 hover:opacity-100"
+            key={data.id}>
+            <a href={`#${data.id}`}>{data.text}</a>
           </li>
         ))}
       </ul>
     </div>
-  )
+  );
 }

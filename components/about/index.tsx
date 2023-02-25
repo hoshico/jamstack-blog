eed84866e462cfb3370f00b8e7725f846f94bf4e
hoshico <melancholinista.yasu@gmail.com>
2023-02-25 18:49:28 +0900

    change bg-color

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -15,29 +15,31 @@ const Rig = ({ v = new Vector3() }) => {
 
 const AboutDetail = () => {
   return (
-    <div className="mx-auto mt-6 w-full rounded-md p-10">
-      <div className="h-[200px]">
-        <Canvas>
-          <ambientLight />
-          <Bath position={[0, -2, 5]} />
-          <Rig />
-          <pointLight position={[10, 10, 10]} />
-          <Text
-            position={[0, 3, 0]}
-            font="/Roboto-Black.ttf"
-            fontSize={3}
-            color={'#fff'}
-          >
-            Profile
-          </Text>
-        </Canvas>
-      </div>
-      <div className="w-full">
-        <p className="text-xl text-white">name: Hoshi</p>
-        <p className="mt-4 text-xl text-white">
-          東京在住の駆け出しエンジニア。
-        </p>
-        <p className="mt-4 text-xl text-white">好きなもの: 銭湯/MLB</p>
+    <div className="min-h-screen bg-gradient-to-br from-cyan-300 to-cyan-800 py-8 px-12">
+      <div className="mx-auto w-full rounded-md bg-gray-400 p-10">
+        <div className="h-[200px]">
+          <Canvas>
+            <ambientLight />
+            <Bath position={[0, -2, 5]} />
+            <Rig />
+            <pointLight position={[10, 10, 10]} />
+            <Text
+              position={[0, 3, 0]}
+              font="/Roboto-Black.ttf"
+              fontSize={3}
+              color={'#fff'}
+            >
+              Profile
+            </Text>
+          </Canvas>
+        </div>
+        <div className="w-full">
+          <p className="text-xl text-white">name: Hoshi</p>
+          <p className="mt-4 text-xl text-white">
+            東京在住の駆け出しエンジニア。
+          </p>
+          <p className="mt-4 text-xl text-white">好きなもの: 銭湯/MLB</p>
+        </div>
       </div>
     </div>
   );

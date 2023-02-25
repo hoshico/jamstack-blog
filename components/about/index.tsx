2535b8d0832de133e74e143986634ca3b275822a
hoshico <melancholinista.yasu@gmail.com>
2023-02-25 23:25:32 +0900

    narrow bg

--- components/about/index.tsx
+++ components/about/index.tsx
@@ -15,9 +15,9 @@ const Rig = ({ v = new Vector3() }) => {
 
 const AboutDetail = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-cyan-300 to-cyan-800 py-8 px-12">
+    <div className="min-h-screen bg-gradient-to-br from-cyan-300 to-cyan-800 py-8 px-12 dark:from-gray-400 dark:to-gray-800">
       {/*プロフィール背景*/}
-      <div className="mx-auto w-full rounded-md bg-stone-800/60">
+      <div className="mx-auto rounded-md bg-stone-800/60 lg:w-6/12">
         {/*プロフィールヘッダー*/}
         <div className="relative flex w-full justify-center text-center">
           <div className="buttonArea absolute left-0 grid w-16 grid-cols-3 gap-4 p-3">
@@ -25,7 +25,7 @@ const AboutDetail = () => {
             <div className="circle h-3 w-3 rounded-full bg-yellow-500"></div>
             <div className="circle h-3 w-3 rounded-full bg-green-500"></div>
           </div>
-          <div className='p-2'>
+          <div className="p-2">
             <span className="text-white">Profile</span>
           </div>
         </div>

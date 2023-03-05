7d7a015ac8c24d1b6af5dd82e77101374a539fbc
hoshico <melancholinista.yasu@gmail.com>
2023-03-05 19:48:08 +0900

    apply BlogDetail

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,60 +1,22 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
-import Moment from 'react-moment';
 import { client } from '../../libs/client';
 import cheerio from 'cheerio';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/hybrid.css';
-import { renderToc } from '../../libs/render-toc';
-import TableOfContents from '../../components/top/TableOfContents';
 import type { Blog } from '../../components/types/Blog';
 import { Params } from 'next/dist/server/router';
+import BlogDetail from '../../components/blog';
 
 type BlogProps = {
   blog: Blog;
   highlightedBody?: string;
 };
 export default function Blog({ blog, highlightedBody }: BlogProps) {
-  const toc = renderToc(blog.body);
   return (
-    // Todo: 記事内もコンポーネント化
-    <div data-theme="winter">
-      <div className="m-auto w-full bg-gradient-to-b from-primary-content via-info to-neutral-content dark:from-base-content dark:to-base-content">
-        <div className="min-h-screen">
-          <div className="py-20">
-            <div className="mx-auto dark:text-base-100 md:max-w-7xl">
-              <div className="px-8 font-extrabold tracking-wider md:text-4xl">
-                {blog.title}
-              </div>
-              <div className="m-auto mt-5 flex px-8 md:w-28 md:px-0">
-                <Moment
-                  className="text-xs text-gray-500 dark:text-base-100"
-                  format="YYYY.MM.DD"
-                >
-                  {blog.publishedAt}
-                </Moment>
-                <p className="text-xs text-gray-500 dark:text-base-100">
-                  に公開
-                </p>
-              </div>
-            </div>
-            {/*メインコンテンツ*/}
-            <div className="mx-auto mt-4  flex max-w-7xl justify-between md:mt-12 md:px-10">
-              {/*本文*/}
-              <div className="w-full bg-base-100 px-8 py-4 shadow dark:bg-slate-800 dark:text-base-100 md:w-[calc(100%_-_288px)] md:rounded-xl md:p-10">
-                <div
-                  className="main-text "
-                  dangerouslySetInnerHTML={{
-                    __html: highlightedBody || '',
-                  }}
-                ></div>
-              </div>
-              {/*サイド*/}
-              <TableOfContents toc={toc} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <BlogDetail
+      blog={blog}
+      highlightedBody={highlightedBody}
+    />
   );
 }
 

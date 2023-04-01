f64c75a15c200c2ac83374ce6c74bfd85f279180
hoshico <melancholinista.yasu@gmail.com>
2023-04-01 15:22:48 +0900

    fix header footer color

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="footer bg-base-200 p-10 text-base-content">
+    <footer className="footer bg-base-300 p-10 text-base-content">
       <div>
         {/*<p>ACME Industries Ltd.Providing reliable tech since 1992</p>*/}
       </div>

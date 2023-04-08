67b2e19f06878ea1c64dd5c72c1a24388c9f8e06
hoshico <melancholinista.yasu@gmail.com>
2023-04-08 14:22:28 +0900

    apply formatDate

--- components/FormatDate.tsx
+++ components/FormatDate.tsx
@@ -3,9 +3,14 @@ import { format } from 'date-fns';
 type Props = {
   dateString: string;
 };
+
 const FormatDate = (props: Props) => {
   const { dateString } = props;
-  console.log(format(new Date(dateString), 'yyyy-MM-dd'));
+  return (
+    <time className="text-xs">
+      {format(new Date(dateString), 'yyyy-MM-dd')}
+    </time>
+  );
 };
 
 export default FormatDate;

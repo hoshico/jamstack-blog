858782f4f184f9290f764c198ce372b2f7915c3d
hoshico <melancholinista.yasu@gmail.com>
2023-04-08 14:32:42 +0900

    apply date-fns-tz

--- components/FormatDate.tsx
+++ components/FormatDate.tsx
@@ -1,4 +1,5 @@
 import { format } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 type Props = {
   dateString: string;
@@ -8,7 +9,7 @@ const FormatDate = (props: Props) => {
   const { dateString } = props;
   return (
     <time className="text-xs">
-      {format(new Date(dateString), 'yyyy-MM-dd')}
+      {format(utcToZonedTime(new Date(dateString), 'Asia/Tokyo'), 'yyyy-MM-dd')}
     </time>
   );
 };

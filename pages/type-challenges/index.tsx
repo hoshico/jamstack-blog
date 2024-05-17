5924340d76d3c8d4306d23a9c00085eb693301e7
hoshico <melancholinista.yasu@gmail.com>
2024-05-17 23:14:48 +0900

    Add medium list

--- pages/type-challenges/index.tsx
+++ pages/type-challenges/index.tsx
@@ -1,26 +1,180 @@
 import TypeChallengeIcon from '../../public/icons/typechallenge.svg';
 
-const challengeList = ['Pick', 'Readonly', 'Tuple to Object', 'First of Array'];
+const easyList = [
+  'Pick',
+  'Readonly',
+  'Tuple to Object',
+  'First of Array',
+  'Length of Tuple',
+  'Exclude',
+  'Awaited',
+  'If',
+  'Concat',
+  'Includes',
+  'Push',
+  'Unshift',
+  'Parameters',
+];
 
-const Typechallenge = () => {
+const mediumList = [
+  'Get Return Type',
+  'Omit',
+  'Readonly',
+  'Deep Readonly',
+  'Turple to Union',
+  'Chainable Options',
+  'Last of Array',
+  'Pop',
+  'Promise.all',
+  'Type Lookup',
+  'Trim Left',
+  'Trim',
+  'Capitalize',
+  'Replace',
+  'ReplaceAll',
+  'Append Argument',
+  'Permutation',
+  'Length of String',
+  'Flatten',
+  'Append to object',
+  'Absolute',
+  'String to Union',
+  'Merge',
+  'KebabCase',
+  'Diff',
+  'AnyOf',
+  'IsNever',
+  'IsUnion',
+  'ReplaceKeys',
+  'Remove Index Signature',
+  'Percentage Parser',
+  'Drop Char',
+  'MinusOne',
+  'PickByType',
+  'StartsWith',
+  'EndsWith',
+  'PartialByKeys',
+  'RequiredByKeys',
+  'Mutabla',
+  'OmitByType',
+  'ObjectEntries',
+  'Shift',
+  'turple to Nested Object',
+  'Reverse',
+  'Flip Arguments',
+  'FlattenDepth',
+  'BEM style string',
+  'InorderTraversal',
+  'Flip',
+  'Fibonacci Sequence',
+  'AllCombinations',
+  'Greater Than',
+  'Zip',
+  'IsTuple',
+  'Chunk',
+  'Fill',
+  'Trim Right',
+  'Without',
+  'Trunc',
+  'IndexOf',
+  'Join',
+  'LastIndexOf',
+  'Unique',
+  'MapTypes',
+  'Construct Tuple',
+  'Number Range',
+  'Combination',
+  'Subsequence',
+  'CheckRepeatedChars',
+  'FirstUniqueCharIndex',
+  'Parse URL Params',
+  'GetMiddleElement',
+  'Appear only once',
+  'Count Element Number To Object',
+  'Integer',
+  'ToPrimitive',
+  'DeepMutable',
+  'All',
+  'Filter',
+  'FindAll',
+  'Combination Key type',
+  'Permutations of Tuple',
+  'Replace First',
+  'Tranpose',
+  'JSON Schema to TypeScript',
+  'Square',
+  'Triangular number',
+  'CartesianProduct',
+  'MergeAll',
+  'CheckRepeatedTuple',
+  'Public Type',
+  'ExtranctToObject',
+  'Deep Omit',
+  'IsOdd',
+  'Tower of hanoi',
+  "Pascal's trianle",
+  'IsFixedStringLiteralType',
+];
+
+// const hardList = [
+//   'Pick',
+//   'Readonly',
+//   'Tuple to Object',
+//   'First of Array',
+//   'Length of Tuple',
+//   'Exclude',
+//   'Awaited',
+//   'If',
+//   'Concat',
+//   'Includes',
+//   'Push',
+//   'Unshift',
+//   'Parameters',
+// ];
+
+const Typechallenges = () => {
   return (
-    <div className="h-dvh bg-white">
+    <div className="h-full bg-white pb-10">
       <div className="flex">
         <div className="mx-auto w-5/6">
           <TypeChallengeIcon />
         </div>
       </div>
       <div className="mt-6">
-        <ul className="mx-auto flex w-5/6 gap-4">
-          {challengeList.map((ele) => (
+        <ul className="mx-auto flex w-5/6 flex-wrap gap-4">
+          {easyList.map((ele) => (
             <li key={ele}>
-              <div className="fit-content">{ele}</div>
+              <div className="badge badge-info gap-2">
+                <p className="text-white">{ele}</p>
+              </div>
             </li>
           ))}
         </ul>
       </div>
+      <div className="mt-6">
+        <ul className="mx-auto flex w-5/6 flex-wrap gap-4">
+          {mediumList.map((ele) => (
+            <li key={ele}>
+              <div className="badge badge-warning gap-2">
+                <p className="text-white">{ele}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {/* <div className="mt-6">
+        <ul className="mx-auto flex w-5/6 flex-wrap gap-4">
+          {hardList.map((ele) => (
+            <li key={ele}>
+              <div className="badge badge-error gap-2">
+                <p className="text-white">{ele}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div> */}
     </div>
   );
 };
 
-export default Typechallenge;
+export default Typechallenges;
